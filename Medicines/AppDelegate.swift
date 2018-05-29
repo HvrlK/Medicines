4f364e75021b4ef335f1f65d4254bdd3053003a3
HvrlK <g.v.s.23.07@gmail.com>
2018-05-29 23:09:51 +0300

    Add image for tab bar items and customize appearance.

--- Medicines/AppDelegate.swift
+++ Medicines/AppDelegate.swift
@@ -27,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     lazy var managedObjectContext: NSManagedObjectContext = self.persistentContainer.viewContext
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        customizeAppearance()
         let defaults = UserDefaults.standard
         if let savedEmail = defaults.string(forKey: "email"), let savedPassword = defaults.string(forKey: "password") {
             let accounts = fetchRequestFromAccounts(managedObjectContext)
@@ -45,6 +46,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func customizeAppearance() {
+        let barTintColor = UIColor(red: 178/255, green: 212/255, blue: 255/255, alpha: 1)
+        UINavigationBar.appearance().barTintColor = barTintColor
+        let titleTextAttributes = [NSAttributedStringKey(rawValue:NSAttributedStringKey.foregroundColor.rawValue): UIColor(red: 23/255, green: 43/255, blue: 77/255, alpha: 1)]
+        UINavigationBar.appearance().largeTitleTextAttributes = titleTextAttributes
+        UITabBar.appearance().barTintColor = barTintColor
+        UITabBar.appearance().tintColor = UIColor(red: 23/255, green: 43/255, blue: 77/255, alpha: 1)
+        UITabBar.appearance().unselectedItemTintColor = UIColor.white
+        window!.tintColor = UIColor(red: 23/255, green: 43/255, blue: 77/255, alpha: 1)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
